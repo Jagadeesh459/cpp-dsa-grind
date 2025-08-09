14e19660ce2d98a3e1b2469ef0c4b06d50d020b6
Jagadeesh459 <njagadish772@gmail.com>
2025-08-09 22:11:13 +0530

    Add files via upload

---
+++ leetcode-solutions/136_single_number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int unique=0;
+        for(int i: nums)
+        {
+            unique=unique^i;
+        }
+        return unique;
+    }
+};
